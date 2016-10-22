06979fac9aaa053c6ba227ecc61602ab9f598f87
Tommy Gaessler <tommy.gaessler@gmail.com>
2016-10-22 16:53:08 -0600

    added category dropdown and chips

--- src/js/components/shop/shop.controller.js
+++ src/js/components/shop/shop.controller.js
@@ -6,11 +6,22 @@
     .module('teaApp.components.shop', [])
     .controller('shopController', shopController);
 
-  shopController.$inject = ['ShopService'];
+  shopController.$inject = ['ShopService', '$rootScope'];
 
-  function shopController(ShopService) {
+  function shopController(ShopService, $rootScope) {
     /* jshint validthis: true */
     this.shop = ShopService.shop();
+    var categories = [];
+    this.shop.forEach(function(tea) {
+      tea.categories.forEach(function(catagory) {
+        if (!categories.length) {
+          categories.push(catagory);
+        } else if (!categories.includes(catagory)) {
+          categories.push(catagory);
+        }
+      });
+    });
+    $rootScope.categories = categories;
   }
 
 })();
